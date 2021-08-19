30c16829685cb3bfc9b3691ee222218e8ab10a1d
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2021-08-19 09:37:13 -0300

    Add files via upload

---
+++ PS2/f78b4d89.cpp
@@ -0,0 +1,159 @@
+//---------------------------------------------------------------------------------------
+// Crockett
+// Date 21/10/2004, Time 13:51:47
+// Martin Fuller: Automatically Produced By ALE Editor
+//---------------------------------------------------------------------------------------
+#ifndef __CROCKETT_H__
+#define __CROCKETT_H__
+
+    //-----------------------------------------------------------------------------------
+    // External Includes
+    //-----------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------
+    // External Defines
+    //-----------------------------------------------------------------------------------
+    #define CROCKETT_ANIM_CROCKETT_1METERCLIMB0008                             8
+    #define CROCKETT_ANIM_CROCKETT_2METERCLIMB0009                             9
+    #define CROCKETT_ANIM_CROCKETT_AIM_ROUND_CORNER_LEFT_0090                  90
+    #define CROCKETT_ANIM_CROCKETT_AIM_ROUND_CORNER_LEFT_IDLE_0091             91
+    #define CROCKETT_ANIM_CROCKETT_AIM_ROUND_CORNER_LEFT_TURNBACK_0092         92
+    #define CROCKETT_ANIM_CROCKETT_AIM_ROUND_CORNER_RIGHT_0093                 93
+    #define CROCKETT_ANIM_CROCKETT_AIM_ROUND_CORNER_RIGHT_IDLE_0094            94
+    #define CROCKETT_ANIM_CROCKETT_AIM_ROUND_CORNER_RIGHT_TURNBACK_0095        95
+    #define CROCKETT_ANIM_CROCKETT_BEING_GRABBED_0121                          121
+    #define CROCKETT_ANIM_CROCKETT_CALLFORHELP0018                             18
+    #define CROCKETT_ANIM_CROCKETT_CLIMB_DOWN_LADDER_0103                      103
+    #define CROCKETT_ANIM_CROCKETT_CLIMB_LADDER_0100                           100
+    #define CROCKETT_ANIM_CROCKETT_CROUCHED_WALK_0145                          145
+    #define CROCKETT_ANIM_CROCKETT_CROUCHED_WALK_BACKWARD_0153                 153
+    #define CROCKETT_ANIM_CROCKETT_DIE_0152                                    152
+    #define CROCKETT_ANIM_CROCKETT_DISMOUNT_LADDER_0101                        101
+    #define CROCKETT_ANIM_CROCKETT_DISMOUNT_LADDER_BOTTOM_0112                 112
+    #define CROCKETT_ANIM_CROCKETT_EXPLOSION_LIGHT_DAMAGE_0200                 200
+    #define CROCKETT_ANIM_CROCKETT_EXPLOSION_LIGHT_GETUP_0201                  201
+    #define CROCKETT_ANIM_CROCKETT_EXTENDED_IDLE_0113                          113
+    #define CROCKETT_ANIM_CROCKETT_FALL0007                                    7
+    #define CROCKETT_ANIM_CROCKETT_GETUP_0125                                  125
+    #define CROCKETT_ANIM_CROCKETT_GIVE_ITEM_0126                              126
+    #define CROCKETT_ANIM_CROCKETT_HEAL_0124                                   124
+    #define CROCKETT_ANIM_CROCKETT_HEAVYLANDING0011                            11
+    #define CROCKETT_ANIM_CROCKETT_HIT0019                                     19
+    #define CROCKETT_ANIM_CROCKETT_IDLE0005                                    5
+    #define CROCKETT_ANIM_CROCKETT_IDLE_AGAINST_WALL_0031                      31
+    #define CROCKETT_ANIM_CROCKETT_JUMP0014                                    14
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_DOWN_0050                             50
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_FIRE_UZI_0154                         154
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_GET_UP_0052                           52
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_IDLE_0051                             51
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_UPPERBODY_FIRE_PISTOL_0156            156
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_UPPERBODY_FIRE_RIFLE_0157             157
+    #define CROCKETT_ANIM_CROCKETT_KNEEL_UPPERBODY_FIRE_UZI_0158               158
+    #define CROCKETT_ANIM_CROCKETT_LIGHTLANDING0010                            10
+    #define CROCKETT_ANIM_CROCKETT_LONG_JUMP_0039                              39
+    #define CROCKETT_ANIM_CROCKETT_LOOK_ROUND_CORNER_LEFT_0027                 27
+    #define CROCKETT_ANIM_CROCKETT_LOOK_ROUND_CORNER_LEFT_IDLE_0028            28
+    #define CROCKETT_ANIM_CROCKETT_LOOK_ROUND_CORNER_LEFT_TURN_BACK_0029       29
+    #define CROCKETT_ANIM_CROCKETT_LOOK_ROUND_CORNER_RIGHT_0024                24
+    #define CROCKETT_ANIM_CROCKETT_LOOK_ROUND_CORNER_RIGHT_IDLE_0025           25
+    #define CROCKETT_ANIM_CROCKETT_LOOK_ROUND_CORNER_RIGHT_TURN_BACK_0026      26
+    #define CROCKETT_ANIM_CROCKETT_MANIPULATE_SWITCH_0130                      130
+    #define CROCKETT_ANIM_CROCKETT_MOUNT_LADDER_0102                           102
+    #define CROCKETT_ANIM_CROCKETT_MOUNT_LADDER_TOP_0110                       110
+    #define CROCKETT_ANIM_CROCKETT_NORMAL_JUMP_0038                            38
+    #define CROCKETT_ANIM_CROCKETT_OPEN_DOOR_0120                              120
+    #define CROCKETT_ANIM_CROCKETT_OPEN_DOOR_SILENT_AWAY_0127                  127
+    #define CROCKETT_ANIM_CROCKETT_OPEN_DOOR_SILENT_TOWARD_0128                128
+    #define CROCKETT_ANIM_CROCKETT_OPEN_DOOR_TOWARD_0129                       129
+    #define CROCKETT_ANIM_CROCKETT_PICKUP_0140                                 140
+    #define CROCKETT_ANIM_CROCKETT_PICKUP_TABLE_DOWN_1042                      1042
+    #define CROCKETT_ANIM_CROCKETT_PICKUP_TABLE_UP_1043                        1043
+    #define CROCKETT_ANIM_CROCKETT_PICKUP_UP_0144                              144
+    #define CROCKETT_ANIM_CROCKETT_RELEASED_0123                               123
+    #define CROCKETT_ANIM_CROCKETT_RUN0004                                     4
+    #define CROCKETT_ANIM_CROCKETT_SHIMMY_TO_IDLE_0030                         30
+    #define CROCKETT_ANIM_CROCKETT_SLIDE_LADDER_0105                           105
+    #define CROCKETT_ANIM_CROCKETT_SLIDE_LADDER_DISMOUNT_0107                  107
+    #define CROCKETT_ANIM_CROCKETT_SNEAKING_SOLO_ARREST_0131                   131
+    #define CROCKETT_ANIM_CROCKETT_STEPDOWN_0013                               13
+    #define CROCKETT_ANIM_CROCKETT_STEPUP0012                                  12
+    #define CROCKETT_ANIM_CROCKETT_STRAFE_BACKWARD_0042                        42
+    #define CROCKETT_ANIM_CROCKETT_STRAFE_FORWARD_0043                         43
+    #define CROCKETT_ANIM_CROCKETT_STRAFE_IDLE_0044                            44
+    #define CROCKETT_ANIM_CROCKETT_STRAFE_LEFT_0040                            40
+    #define CROCKETT_ANIM_CROCKETT_STRAFE_RIGHT_0041                           41
+    #define CROCKETT_ANIM_CROCKETT_STRAFFE_LEFT_AGAINST_WALL_0022              22
+    #define CROCKETT_ANIM_CROCKETT_STRAFFE_RIGHT_AGAINST_WALL_0021             21
+    #define CROCKETT_ANIM_CROCKETT_STRUGGLE_0122                               122
+    #define CROCKETT_ANIM_CROCKETT_TURN_TO_WALL_0020                           20
+    #define CROCKETT_ANIM_CROCKETT_UNARMED_SNEAK_0106                          106
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_AIM_0072                          72
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_AIM_RIFLE_0147                    147
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_AIM_UZI_0074                      74
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_DAMAGE_0141                       141
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_FIRE_0070                         70
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_FIRE_RIFLE_0148                   148
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_FIRE_UZI_0075                     75
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_HOLD_PISTOL_0078                  78
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_HOLD_RIFLE_0149                   149
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_HOLD_TWO_HANDED_0079              79
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_HOLSTER_0076                      76
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_HOLSTER_RIFLE_0155                155
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_RELOAD_0071                       71
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_RELOAD_RIFLE_0150                 150
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_RELOAD_UZI_0073                   73
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_UNHOLSTER_PISTOL_0146             146
+    #define CROCKETT_ANIM_CROCKETT_UPPERBODY_UNHOLSTER_RIFLE_0154              1953789044
+    #define CROCKETT_ANIM_CROCKETT_WALK0001                                    1
+
+    #define CROCKETT_TEXTURE_MV_JB_CROCKHANDSB                                 0x351d6650 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_SKIN_SPEC_02                                      0xe1cd0ef4 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MV_JB_CROCKHOLSTER                                0xf1a29d4b 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_SPEC2                                             0x9f6c2acc 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MV_JB_CROCKGLASSB                                 0xa7a57194 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_CROCK_HAIRB                                       0xbbdd6db4 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MOUTH_TEETH                                       0xc1452e3c 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_CROCK15B                                          0xedcabf4a 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MV_JB_CROCKTREWS2B                                0x7d4937c8 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_SUBTLESPEC                                        0x7c0509ed 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MV_JB_CROCKT02B                                   0x68b4e31c 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_CROK_VEST_SPEC_01                                 0xa2adc6e8 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MV_JB_CROCKARM02                                  0x2d61fb37 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_MV_JB_CROCKSHOE02                                 0x870d7894 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_EYEBL                                             0x34adfb96 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_TESTSHADER01                                      0x79f152a0 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_REFLECT_SHINY_STRIPE_MOD                          0x72134445 	// Geometry Texture, 
+    #define CROCKETT_TEXTURE_REFLECT_SHINY_STRIPE_ADD                          0x131b22ba 	// Geometry Texture, 
+
+    #define CROCKETT_GEOMETRY_CROCKETT_SKIN_2                                  0x6ca3f709
+
+    #define CROCKETT_HULL_CROCKETT                                             0x6c296087
+
+    // Hull: Crockett
+    #define CROCKETT_INSTANCE_CROCKETT_SKIN                                    0x97a3dc97
+
+    //-----------------------------------------------------------------------------------
+    // External Enums
+    //-----------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------
+    // External Types
+    //-----------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------
+    // External Variables
+    //-----------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------
+    // External functions
+    //-----------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------
+    // Inline functions
+    //-----------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------
+    // Additional includes that depend on this header
+    //-----------------------------------------------------------------------------------
+
+#endif // __CROCKETT_H__
